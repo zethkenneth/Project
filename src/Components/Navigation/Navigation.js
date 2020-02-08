7bff0fb226853ef91428bdf776c8621ec9762cae
zethkenneth <pungtodzethkenneth@gmail.com>
2020-02-08 13:58:55 +0800

    First commit

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import wmsu from './wmsu.png';
+
+const Navigation = () => {   
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="navbar-brand" >
+                <img src={wmsu} width="30" height="30" className="d-inline-block align-top" alt="..." /><strong>WMSU</strong> eClinic
+            </div>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav ml-auto text-right">
+                        <li className="nav-item active">
+                            <div className="nav-link" >Home <span className="sr-only">(current)</span></div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" >About Us</div>
+                        </li>
+                        <li className="nav-item">
+                            <button type="button" className="btn btn-outline-primary">Log in</button>
+                        </li>
+                    </ul>
+                </div> 
+         </nav>
+    );
+}
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'jquery/src/jquery'; 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -10,3 +15,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+    
